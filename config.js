d7f0906cebfe168cddf906322e171110329053a7
xShawnacy <63955859+xShawnacy@users.noreply.github.com>
2026-01-14 11:51:37 +0100

    Update PARIS_FORM_URL to new Google Forms link

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 const CONFIG = {
 AUTO_COTES_URL: "https://docs.google.com/spreadsheets/d/e/2PACX-1vTAbDtfK4pEAlfBlo6JrSA5dcGV35C1oEF37DUUF-oP6n5dk81yGmNuPnKYxhBGsg/pub?gid=111010267&single=true&output=csv",
 HISTORY_URL: "https://docs.google.com/spreadsheets/d/e/2PACX-1vTAbDtfK4pEAlfBlo6JrSA5dcGV35C1oEF37DUUF-oP6n5dk81yGmNuPnKYxhBGsg/pub?gid=1714782319&single=true&output=csv",
-PARIS_FORM_URL: "https://docs.google.com/forms/d/e/1FAIpQLSf6iIBNwrmNEWtB-nVNaJM9-5X5LTger4O-z0S7yVO-FI9l6w/viewform?usp=sf_link",
+PARIS_FORM_URL: "https://forms.gle/fyJ3yk6LbyzFJR5S9",
 DEBT_FORM_URL: "URL_GOOGLE_FORM_DETTES",
 ADMIN_PASSWORD: "jokeradmin",
 ILLEGAL_PASSWORD: "blackmarket",
