804d663d8b30ed1cb4a3ca0aeea648b1a7fec6a3
xShawnacy <63955859+xShawnacy@users.noreply.github.com>
2026-01-14 12:31:08 +0100

    Update configuration with new sheet ID and name

--- config.js
+++ config.js
@@ -1,5 +1,6 @@
 const CONFIG = {
-AUTO_COTES_URL: "https://docs.google.com/spreadsheets/d/e/2PACX-1vTAbDtfK4pEAlfBlo6JrSA5dcGV35C1oEF37DUUF-oP6n5dk81yGmNuPnKYxhBGsg/pub?gid=266215270&single=true&output=csv",
+SHEET_ID: "1lGevgn5Z-sxdnW0bZG3t10WezDYuHMQT",
+SHEET_NAME: "AUTO_COTES",
 HISTORY_URL: "https://docs.google.com/spreadsheets/d/e/2PACX-1vTAbDtfK4pEAlfBlo6JrSA5dcGV35C1oEF37DUUF-oP6n5dk81yGmNuPnKYxhBGsg/pub?gid=1714782319&single=true&output=csv",
 PARIS_FORM_URL: "https://forms.gle/fyJ3yk6LbyzFJR5S9",
 DEBT_FORM_URL: "URL_GOOGLE_FORM_DETTES",
